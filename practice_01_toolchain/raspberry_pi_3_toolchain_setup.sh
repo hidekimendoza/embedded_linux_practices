dce3a10c3562717b4e9f8b78ba05caa86eb45cd5
hideki_mendoza <hideki.mendoza@hotmail.com>
2019-08-13 00:49:02 -0700

    [practice 01 toolchain] Fix: Move functions to the top of the file, running shellcheck and fixing functionality.
    - Redirect stderr instead of stout for hash command
    - fix associative array definition
    - Move functions to the top of the file
    - Run shellcheck

--- practice_01_toolchain/raspberry_pi_3_toolchain_setup.sh
+++ practice_01_toolchain/raspberry_pi_3_toolchain_setup.sh
@@ -21,33 +21,11 @@
 #    Texinfo-4.13
 #    Sed-4.2.1
 
-declare -A standard_tools { 
-[bash]="4.0"
-[binutils]="2.20"
-[bzip2]="1.0.5"
-[coreutils]="8.1"
-[diffutils]="3.0"
-[findutils]="4.4.0"
-[gawk]="3.1"
-[gcc]="4.4"
-[glibc]="2.11"
-[grep]="2.6"
-[gzip]="1.3"
-[m4]="1.4.16"
-[make]="3.81"
-[makeinfo]="4.13"
-[ncurses]="5"
-[patch]="2.6"
-[tar]="1.23"
-[sed]="4.2.1"
-[sudo]="1.7.4p4"
-}
-
 function is_installed(){
   local tool="$1"
 
-  is_installed=$(hash "${tool}" 1>/dev/null)
-  echo "${is_installed}"
+  hash "${tool}" 2>/dev/null
+  return $?
 }
 
 function evaluate_corresponding_version(){
@@ -61,42 +39,89 @@ function evaluate_corresponding_version(){
    fi
 }
 
+function get_version(){
+  local app="$1"
+  local app_version
+  case ${app} in
+  "bash")
+    app_version=$(bash --version | head -n1 | cut -d" " -f2-4)
+    ;;
+  "binutils")
+    app_version=$(ld --version | head -n1 | cut -d" " -f3-)
+    ;;
+  "bzip")
+    app_version=$(bzip2 --version 2>&1 < /dev/null | head -n1 | cut -d" " -f1,6-)
+    ;;
+  "coreutils")
+    app_version=$(chown --version | head -n1 | cut -d")" -f2)
+    ;;
+  "glibc")
+    app_version=$(ldd "$(which "${SHELL}")" | grep libc.so | cut -d ' ' -f 3 \
+  	  | "${SHELL}" | head -n 1 | cut -d ' ' -f 1-10)
+    ;;
+  "ncurses")
+    app_version=$(echo "#include <ncurses.h>" | gcc -E - > /dev/null)
+    ;;
+  *)
+    app_version=$(${app} --version | head -n1)
+  esac
+  echo "${app_version}"
+}
+
 function manage_version_installed(){
   local util_name="$1"
   local expected_version="$2"
 
-  if $(is_installed "${util_name}"); then
-    if $(evaluate_corresponding_version "${util_name}" "${expected_version}") ; then
+  if is_installed "${util_name}"; then
+    current_version=$(get_version "${util_name}")
+    if evaluate_corresponding_version "${current_version}" "${expected_version}" ; then
+      echo "${util_name} tool installed"
       return 0
     else
+      echo "${util_name} tool need upgrade"
       return 1
     fi
   else
+    echo "${util_name} tool is not installed"
     return 2
   fi
 }
 
-function get_version(){
+function update_tool(){
+  local app="$1"
   case ${app} in
-  "bash")
-    echo "$(bash --version | head -n1 | cut -d" " -f2-4)" 
-  "binutils")
-    echo "$(ld --version | head -n1 | cut -d" " -f3-)"
-    ;;
-  "bzip")
-    echo "$(bzip2 --version 2>&1 < /dev/null | head -n1 | cut -d" " -f1,6-)"
-    ;;
-  "coreutils")
-    echo "$(chown --version | head -n1 | cut -d")" -f2)"
-    ;;
-  "glibc")
-    echo "$(ldd "$(which "${SHELL}")" | grep libc.so | cut -d ' ' -f 3 \
-  	  | "${SHELL}" | head -n 1 | cut -d ' ' -f 1-10)"
-    ;;
-  "ncurses")
-    echo "$(echo "#include <ncurses.h>" | gcc -E - > /dev/null)"
-    ;;
   *)
-    echo "$(${app} --version | head -n1)"
+    sudo apt-get install "${app}"
   esac
 }
+
+manage_version_installed 'bash' '4.0'
+
+declare -A standard_tools=(
+['binutils']="2.20"
+['bzip2']="1.0.5"
+['coreutils']="8.1"
+['diff']="3.0"
+['find']="4.4.0"
+['gawk']="3.1"
+['gcc']="4.4"
+['glibc']="2.11"
+['grep']="2.6"
+['gzip']="1.3"
+['m4']="1.4.16"
+['make']="3.81"
+['makeinfo']="4.13"
+['ncurses']="5"
+['patch']="2.6"
+['tar']="1.23"
+['sed']="4.2.1"
+['sudo']="1.7.4p4"
+)
+
+for tool in "${!standard_tools[@]}"; do
+  expected_version=${standard_tools[$tool]}
+
+  echo "Checking util ${tool}"
+  manage_version_installed "${tool}" "${expected_version}"
+done
+
