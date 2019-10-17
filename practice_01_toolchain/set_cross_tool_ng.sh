a04954451c551846a8a695e1ab9d8aaab3ccd415
hideki_mendoza <hideki.mendoza@hotmail.com>
2019-10-16 23:34:45 -0700

    [practice 01 toolchain] adding ct-ng install script
    Adding as part of practice 01 crosstool ng script
    which pull and install requirements and tool.

---
+++ practice_01_toolchain/set_cross_tool_ng.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Based on http://crosstool-ng.github.io/docs/build/
+VERSION="1.9.3"
+
+function install_ct_ng(){
+
+  echo -n "Do you want to install required ubuntu packages? [y/n]: "
+  read -n 1 ans
+  
+  if [[ "${ans^^}" == 'Y' ]]; then
+    sudo apt-get install -y gcc g++ gperf \
+      bison flex texinfo help2man make \
+      libncurses5-dev libncursesw5-dev \
+      python3-dev autoconf automake libtool \
+      libtool-bin gawk wget bzip2 xz-utils unzip \
+      patch libstdc++6 cvs
+  fi
+
+  mkdir -p ct-ng_setup
+  cd ct-ng_setup || exit 1
+  
+  mkdir -p sources
+  cd sources || exit 1
+  # First, download the tarball:
+  wget http://crosstool-ng.org/download/crosstool-ng/crosstool-ng-"${VERSION}".tar.bz2
+  tar -xf crosstool-ng-"${VERSION}".tar.bz2 && rm crosstool-ng-"${VERSION}".tar.bz2
+  
+  # Requirements
+  # https://github.com/crosstool-ng/crosstool-ng.git -> 
+  # crosstool-ng/testing/docker/ubuntu18.04/Dockerfile
+
+  # RUN wget -O /sbin/dumb-init https://github.com/Yelp/dumb-init/releases/download/v1.2.1/dumb-init_1.2.1_amd64
+  # RUN chmod a+x /sbin/dumb-init
+  # RUN echo 'export PATH=/opt/ctng/bin:$PATH' >> /etc/profile
+  # ENTRYPOINT [ "/sbin/dumb-init", "--" ]
+  
+  cd crosstool-ng-"${VERSION}" || exit 1 
+  
+  ./configure --local
+#   make
+#   
+#   if ! ./ct-ng help ;then
+#     echo "ct-ng was not successfully installed"
+#     exit 1
+#   fi
+#   
+#   ./configure --prefix=/usr
+#   make
+#   make DESTDIR=/packaging/place install
+# 
+#   ct-ng menuconfig
+}
+
+install_ct_ng
